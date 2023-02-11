b07b14f44ba90a3af6d3a5c1d408a174a80dce4a
Shizuka Donaghue <sdonaghue@gmail.com>
2023-02-11 18:59:48 +0000

    First and last commit

--- evens.py
+++ evens.py
@@ -8,7 +8,18 @@ def even_number_of_evens(numbers):
     if the number of even numbers is even, return True
     """
 
-    return None
+    if isinstance(numbers, list):
+        evens = sum([1 for n in numbers if n % 2 == 0])
+
+        return True if evens and evens % 2 == 0 else False
+
+        # if evens:
+        #     return evens % 2 == 0
+        # else:
+        #     return False
+    else:
+        raise TypeError("A list was not passed into the function")
 
 
-print(even_number_of_evens(5))
+if __name__ == '__main__':
+    even_number_of_evens([2, 1, 4])
